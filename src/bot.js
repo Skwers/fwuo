262ea13d2ababadfa3c18a5012fe67ce9c5d21ee
Skwers <73722883+Skwers@users.noreply.github.com>
2020-11-15 21:43:13 -0500

    Removed dotenv config

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,12 @@
 "use strict";
 
-const path = require("path");
-
 const { Client } = require("discord.js");
-require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 (async () => {
   const client = new Client();
   client.commands = require("./commands");
   client.events = require("./events");
+  client.database = require("./database");
 
   client.removeAllListeners();
 
